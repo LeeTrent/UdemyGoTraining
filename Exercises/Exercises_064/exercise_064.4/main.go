21c7212bb7545aa60f0fa76ed34017bf41ca6a13
LeeTrent <Lee.Trent.1@gmail.com>
2016-03-15 22:56:21 -0500

    adds exercise_064.4

---
+++ Exercises/Exercises_064/exercise_064.4/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    var smallNumber int
+    var largeNumber int
+    
+    fmt.Print("Enter small number: ")
+    fmt.Scan(&smallNumber)
+    
+    fmt.Print("Enter large number: ")
+    fmt.Scan(&largeNumber)
+    
+    if smallNumber != 0 {
+        fmt.Println("Remainder is", largeNumber % smallNumber)
+    } else {
+        fmt.Println("Cannot divide by zero")
+    }
+}
